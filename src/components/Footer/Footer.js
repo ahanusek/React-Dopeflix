4192b70e628ad88546f74968bdef43dd245f8288
folxu <adambugaj3@gmail.com>
2018-12-14 16:31:23 +0100

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/folxu/">GH</a>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Main/More/More.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const More = () => {
+  return (
+    <section>
+      <h1>More page</h1>
+    </section>
+  )
+}
+
+export default More;
