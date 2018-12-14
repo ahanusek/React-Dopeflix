ab844761e7f290a28fe415d94f43cd9a3818b45d
folxu <adambugaj3@gmail.com>
2018-12-14 13:57:06 +0100

    Add some styles

--- src/components/Main/Trending-output.js
+++ src/components/Main/Trending-output.js
@@ -3,26 +3,17 @@ import "./main.scss"
 
 const TrendingOutput = (props) => {
 
-  let description;
-  // console.log(description.length);
-
-  if(window.outerWidth < 1024){
-    description = props.description.slice(0, 320);
-  }
-  else{
-    description = props.description;
-  }
-
   return (
     <div className="trending"
       style={{
+        // eslint-disable-next-line
         backgroundImage: "url(" + `https://image.tmdb.org/t/p/original${props.picture}` + ")"
       }}>
 
       <div className="trending-bg">
         <div className="trending-container">
           <p className="movie-title">{props.movieName}</p>
-          <p className="movie-description">{description}</p>
+          <p className="movie-description">{props.description}</p>
           <p className="movie-text">Premiera: <span>{props.releaseDate}</span></p>
           <p className="movie-text">Liczba głosów: <span>{props.ratingCount}</span></p>
           <p className="movie-text">Średnia: <span>{props.rating}</span></p>
