ee79bbcfc71e1776678e1a3fac7f784962eea6f5
folxu <adambugaj3@gmail.com>
2018-12-15 13:19:17 +0100

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import tmdbLogo from "../../tmdb-logo.png";
 import "./footer.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/folxu/">GH</a>
+      <div className="footer-container">
+        <div>
+          <p className="footer-brand-logo">
+            <Link to="/">Dopeflix</Link>
+          </p>
+          <p>Wszystkie filmy i seriale <br /> w jednym, miejscu</p>
+        </div>
+
+        <div className="footer-credits">
+          <p>
+            <a href="http://github.com/folxu/">
+              Created by folxu.
+            </a>
+          </p>
+        </div>
+
+        <div>
+          <img className="tmdb-logo" src={tmdbLogo} alt="TheMobieDB Logo" />
+        </div>
+      </div>
     </footer>
   )
 }
